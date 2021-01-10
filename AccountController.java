b7285dce7c767e8a914ecf6b7e0ea8fa4ef0877d
adhikarip <pushpa_raj_a@hotmail.com>
2021-01-10 14:42:14 -0500

    Add files via upload

---
+++ AccountController.java
@@ -0,0 +1,14 @@
+package intelligent;
+
+ 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AccountController {
+
+	@RequestMapping("/")
+	public String index() {
+		return "Greetings from Spring Boot!";
+	}
+}
